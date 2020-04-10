63c9193d31b89adf0edc53004017c7cef7f0a9c4
Paul Cortes <paulkerg@gmail.com>
2020-04-09 19:37:06 -0500

    fixed view expenseReport

--- resources/views/expenseReport/index.blade.php
+++ resources/views/expenseReport/index.blade.php
@@ -5,6 +5,16 @@
 <div class="row">
     <div class="col">
         <h1>Reports</h1>
+    </div>
+</div>
+<div class="row">
+    <div class="col">
+
+    </div>
+</div>
+
+<div class="row">
+    <div class="col">
         <a href="/expense_reports/create">Create a new report</a>
         <table>
             @foreach($expenseReports as $expenseReport)
