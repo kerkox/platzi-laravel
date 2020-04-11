02659e10bed72c854e088cc356dfcd9fdc259827
Paul Cortes <paulkerg@gmail.com>
2020-04-11 16:19:02 -0500

    add relations between Expense Model and ExpenseReport Model

--- app/ExpenseReport.php
+++ app/ExpenseReport.php
@@ -2,9 +2,13 @@
 
 namespace App;
 
+use App\Expense;
 use Illuminate\Database\Eloquent\Model;
 
 class ExpenseReport extends Model
 {
-
+    public function expenses()
+    {
+        return $this->hasMany(Expense::class);
+    }
 }
