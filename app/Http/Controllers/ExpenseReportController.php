aa127011505f5ddbcf7cdc9914130971514b1566
Paul Cortes <paulkerg@gmail.com>
2020-04-11 16:05:14 -0500

    add view show expenseReport, and Model bindin in method show of ExpenseReportController

--- app/Http/Controllers/ExpenseReportController.php
+++ app/Http/Controllers/ExpenseReportController.php
@@ -54,9 +54,11 @@ class ExpenseReportController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(ExpensenseReport $expenseReport)
     {
-        //
+        return view('expenseReport.show', [
+            'report' => $expenseReport
+        ]);
     }
 
     /**
