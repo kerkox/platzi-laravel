33a6c90949b4c37d5eaa09315625b3cc8ab59c60
Paul Cortes <paulkerg@gmail.com>
2020-04-11 22:35:45 -0500

    change confirmDelete view

--- resources/views/expenseReport/confirmDelete.blade.php
+++ resources/views/expenseReport/confirmDelete.blade.php
@@ -1,33 +1,35 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="row justify-content-center">
+    <div class="col-md-8">
+        <div class="card">
+            <div class="card-header">Reports</div>
+            <div class="card-body">
+                <div class="row">
+                    <div class="col">
+                        <h1>Delete Report {{ $report->id }}</h1>
+                    </div>
+                </div>
 
-<div class="row">
-    <div class="col">
-        <h1>Delete Report {{ $report->id }}</h1>
-    </div>
-</div>
+                <div class="row">
+                    <div class="col">
+                        <a class="btn btn-secondary" href="/expense_reports">Back</a>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col mt-3">
+                        <form action="/expense_reports/{{ $report->id }}" method="POST">
+                            @csrf
+                            @method('delete')
 
-<div class="row">
-    <div class="col">
-        <a class="btn btn-secondary" href="/expense_reports">Back</a>
-    </div>
-</div>
-<div class="row">
-    <div class="col">
+                            <button class="btn btn-danger" type="submit">Delete</button>
+                        </form>
 
-    </div>
-</div>
-
-<div class="row">
-    <div class="col">
-        <form action="/expense_reports/{{ $report->id }}" method="POST">
-            @csrf
-            @method('delete')
-
-            <button class="btn btn-danger" type="submit">Delete</button>
-        </form>
-
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 @endsection
