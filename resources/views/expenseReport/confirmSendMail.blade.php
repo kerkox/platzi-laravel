f08a5ce9e137a59724f57b9a4ff6f31b234c51b8
Paul Cortes <paulkerg@gmail.com>
2020-04-11 22:34:50 -0500

    change confirmSendMail view

--- resources/views/expenseReport/confirmSendMail.blade.php
+++ resources/views/expenseReport/confirmSendMail.blade.php
@@ -1,45 +1,54 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="row justify-content-center">
+    <div class="col-md-8">
+        <div class="card">
+            <div class="card-header">Reports</div>
+            <div class="card-body">
 
-<div class="row">
-    <div class="col">
-        <h1>Send Report: {{ $report->title }}</h1>
-    </div>
-</div>
+                <div class="row">
+                    <div class="col">
+                        <h1>Send Report: {{ $report->title }}</h1>
+                    </div>
+                </div>
 
-<div class="row">
-    <div class="col">
-        <a class="btn btn-secondary" href="/expense_reports/{{ $report->id }}">Back</a>
-    </div>
-</div>
-<div class="row">
-    <div class="col">
+                <div class="row">
+                    <div class="col">
+                        <a class="btn btn-secondary" href="/expense_reports/{{ $report->id }}">Back</a>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
 
-    </div>
-</div>
+                    </div>
+                </div>
 
-<div class="row">
-    <div class="col">
-        @if($errors->any())
-        <div class="alert alert-danger">
-            <ul>
-                @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-        @endif
-        <form action="/expense_reports/{{ $report->id }}/sendMail" method="POST">
-            @csrf
-            <div class="form-group">
-                <label for="email">Title:</label>
-                <input type="email" class="form-control" id="email" name="email" placeholder="Email"
-                    value="{{ old('email') }}">
+                <div class="row">
+                    <div class="col">
+                        @if($errors->any())
+                        <div class="alert alert-danger">
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                        @endif
+                        <form action="/expense_reports/{{ $report->id }}/sendMail" method="POST">
+                            @csrf
+                            <div class="form-group">
+                                <label for="email">Title:</label>
+                                <input type="email" class="form-control" id="email" name="email" placeholder="Email"
+                                    value="{{ old('email') }}">
+                            </div>
+                            <button class="btn btn-primary" type="submit">Send mail</button>
+                        </form>
+
+                    </div>
+                </div>
             </div>
-            <button class="btn btn-primary" type="submit">Send mail</button>
-        </form>
-
+        </div>
     </div>
 </div>
 @endsection
