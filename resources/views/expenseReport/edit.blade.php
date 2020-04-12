f8572e019d6366b6cd36f083803b5f87c4d90d5e
Paul Cortes <paulkerg@gmail.com>
2020-04-11 22:21:44 -0500

    change edit view

--- resources/views/expenseReport/edit.blade.php
+++ resources/views/expenseReport/edit.blade.php
@@ -1,36 +1,39 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="row justify-content-center">
+    <div class="col-md-8">
+        <div class="card">
+            <div class="card-header">Reports</div>
+            <div class="card-body">
+                <div class="row">
+                    <div class="col">
+                        <h1>Edit Report {{ $report->id }}</h1>
+                    </div>
+                </div>
 
-<div class="row">
-    <div class="col">
-        <h1>Edit Report {{ $report->id }}</h1>
-    </div>
-</div>
+                <div class="row">
+                    <div class="col">
+                        <a class="btn btn-secondary" href="/expense_reports">Back</a>
+                    </div>
+                </div>
 
-<div class="row">
-    <div class="col">
-        <a class="btn btn-secondary" href="/expense_reports">Back</a>
-    </div>
-</div>
-<div class="row">
-    <div class="col">
+                <div class="row">
+                    <div class="col mt-3">
+                        <form action="/expense_reports/{{ $report->id }}" method="POST">
+                            @csrf
+                            @method('put')
+                            <div class="form-group">
+                                <label for="title">Title:</label>
+                                <input type="text" class="form-control" id="title" name="title" placeholder="type a title" value="{{ old('title') }}">
+                            </div>
+                            <button class="btn btn-primary" type="submit">Submit</button>
+                        </form>
 
-    </div>
-</div>
-
-<div class="row">
-    <div class="col">
-        <form action="/expense_reports/{{ $report->id }}" method="POST">
-            @csrf
-            @method('put')
-            <div class="form-group">
-                <label for="title">Title:</label>
-                <input type="text" class="form-control" id="title" name="title" placeholder="type a title" value="{{ old('title') }}">
+                    </div>
+                </div>
             </div>
-            <button class="btn btn-primary" type="submit">Submit</button>
-        </form>
-
+        </div>
     </div>
 </div>
 @endsection
